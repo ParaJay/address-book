7e915e7b941847267a8090e60697227390e0e62e
ParaJay <calumjhunter@gmail.com>
2023-03-11 01:42:11 +0000

    contacts are now sorted properly

--- src/Components/ContactsList.js
+++ src/Components/ContactsList.js
@@ -1,15 +1,31 @@
 import Contact from "./Contact";
 
-function ContactsList(props) {
+const compareString = (a, b) => {
+    if(a === b) return 0;
+
+    for(let i = 0; i < a.length; i++) {
+        if(i >= b.length) return -1;
+        let aa = a.charAt(i);
+        let bb = b.charAt(i);
+
+        if(aa > bb) {
+            return 1;
+        } else if(aa < bb) {
+            return -1;
+        }
+    }
+
+    return 1;
+}
+
+const ContactsList = (props) => {
     let charMap = {};
     let components = [];
 
-    let contacts = props.contacts.sort();
+    let contacts = props.contacts.sort((a, b) => compareString(a.name, b.name));
 
     for(let i = 0; i < contacts.length; i++) {
         let contact = contacts[i].name;
-
-        console.log(contact);
 
         let char = contact.charAt(0).toUpperCase();
 
@@ -29,7 +45,7 @@ function ContactsList(props) {
     for(let i = 0; i < keys.length; i++) {
         let key = keys[i];
 
-        charMap[key].unshift(<h3 key="h">{key}</h3>)
+        charMap[key].unshift(<h3 className="contact-header" key="h">{key}</h3>)
         components.push(charMap[key]);
     }
 
