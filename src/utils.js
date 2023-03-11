7c2a51bd07776a7cb623764765500d3c5692d33c
ParaJay <calumjhunter@gmail.com>
2023-03-11 01:46:13 +0000

    added names list for faking, randoms

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export class Random {
-    constructor() {
-
+    constructor(inclusive = true) {
+        this.inclusive = inclusive;
     }
 
     /**
@@ -9,7 +9,7 @@ export class Random {
      * @returns a number between 0 and int
      */
     nextInt(int) {
-        return this.randint(0, int);
+        return this.randint(0, int - (this.inclusive ? 0 : 1));
     }
 
     /**
@@ -25,8 +25,33 @@ export class Random {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    randfloat(min, max) {
-        return Math.random() * (max - min) + min;
+    intFromArray(array) {
+        return this.nextInt(array.length - 1);
+    }
+
+    fromArray(array) {
+        return array[this.intFromArray(array)];
+    }
+
+    randchance(array, chances) {
+        let size = 0;
+    
+        for(let i = 0; i < array.length; i++) {   
+            size += chances[i];
+        }
+    
+        let random = new Random().nextInt(size);
+        let probability = 0;
+    
+        for(let i = 0; i < array.length; i++) {   
+            probability += chances[i];
+    
+            if(random <= probability) {
+                return array[i];
+            }
+        }
+        
+        return array[0].randomize();
     }
 }
 
@@ -85,3 +110,5 @@ function shuffle(array) {
 export function isUUID(uuid) {
     return uuid.match("[0-9a-zA-Z]{8}(-{1}[0-9a-zA-Z]{8}){3}");
 }
+
+export const names = "Ayla Clements\nWanda Young\nAran Connor\nBridget Sweeney\nSonia Hurst\nCrystal Harrell\nLester Delacruz\nLeena Anthony\nMartina Austin\nSana Contreras\nMaisie Mendez\nGabriella Wood\nShayla Carson\nHenrietta Johnson\nAnish Delgado\nMurray Lozano\nVivian Waters\nAlexa Campos\nAlexandros Figueroa\nRhianna Haney\nRobert Hayden\nDawn Campbell\nTheodore Carpenter\nJude Soto\nMarie Shelton\nZayn Sims\nAbbas Meyers\nYoussef Whitaker\nMaja Cline\nWendy Gentry\nHaider Price\nDafydd Rich\nEva Kramer\nErika Lopez\nBella Solis\nSafa Carter\nLucian Thomas\nCohen Holder\nBeatrix Carroll\nEvangeline Harmon\nRafe Santos\nJazmine Alvarado\nBruce Griffith\nAnisa Fulton\nJack English\nIda Matthams\nNoor Kane\nRhodri Gillespie\nAaminah Skinner\nNiall George\nKajus Spencer\nBruno Winters\nOskar Finch\nDanielle Case\nCelia Odling\nTiana Burns\nMegan Higgins\nAudrey Howard\nJulie Douglas\nElisa Dickerson\nKrishan Hartman\nKaleb Norman\nBibi Mercado\nEleni Blaese\nIvan Osborne\nSapphire Jones\nKadie Dixon\nStacey Day\nLeonie Meyer\nTyrell Daugherty\nBridie Greer\nKyan Valentine\nLila Oconnell\nKain Ponce\nTim Owen\nKeaton Jennings\nSaarah Parrish\nJessie Schneider\nMahdi Parsons\nCallan Peters\nIsobelle Gordon\nRosemary Sloan\nNana Haynes\nMark Rubio\nPhoenix Salinas\nJuanita Fernandez\nKatelyn Barker\nAlbert Melton\nAngus Quinn\nChaya Mathis\nIsobel Rollins\nMarilyn Griffin\nEarl Yoder\nBrendon Hunt\nAliyah Lam\nAddie Graham\nGilbert Ayala\nRehan Rangel\nKristian Daniel\nHollie Mcpherson".split("\n");
